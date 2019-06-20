0bbf2916f28459843909a003d24a44e0b7e8feeb
Comcx <comcx@outlook.com>
2019-06-20 20:17:13 +0800

    port

--- Port.h
+++ Port.h
@@ -1,8 +1,18 @@
 #ifndef PORT_H
 #define PORT_H
 
+#include "Types.h"
 
 
+inline uint8_t  in8      (uint16_t portNum);
+inline void     out8     (uint16_t port, uint8_t data);
+inline void     out8Slow (uint16_t port, uint8_t data);
+
+inline uint16_t in16     (uint16_t port);
+inline void     out16    (uint16_t port, uint16_t data);
+
+inline uint32_t in32     (uint16_t port);
+inline void     out32    (uint16_t port, uint32_t data);
 
 
 #endif
